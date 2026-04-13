f1fffaf293ecce675c71ad909f3e7771c7e60fd1
danielgonzalez150 <daniel.gonzalez02@unicatolica.edu.co>
2026-04-13 14:32:23 -0500

    Agregué el login: - Creé la función de login en auth_repository. - Creé una plantilla de html para que el usuario se pueda logear. - Creé los endpoints para que se conecte el front con el back.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from fastapi.templating import Jinja2Templates
 # Importar el objeto de respuesta HTML para mostrar el HTML
 from fastapi.responses import HTMLResponse
 
-# Importar la función de registrar usuario
-from database.auth_repository import register_new_user
+# Importar el CRUD del usuario (Registro y logeo)
+from database.auth_repository import register_new_user, login_user
 
 # Creamos el objeto de FastAPI y el objeto de plantilas
 app = FastAPI()
@@ -61,3 +61,38 @@ async def post_register(
         "request": request, 
         "mensaje": mensaje
     })
+
+@app.get("/login", response_class=HTMLResponse)
+async def get_login(request: Request):
+    """
+    Función para renderizar la página de login.
+    """
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/auth/login")
+async def post_login(request: Request, email: str = Form(...), password: str = Form(...)):
+    """
+    Método POST para realizar el login del usuario.
+
+    Entrada:
+        - emaiL: Correo del usuario del formulario.
+        - password: Contraseña del usuario del usuario
+
+    Retorna:
+        - Renderiza el template del index sí se logea exitosamente, sino
+        renderiza nuevamente el login.
+
+    """
+    # LLamar a la función CRUD para el logeo
+    resultado = login_user(email, password)
+    
+    # Checar que haya resultado y que sea un usuario válido
+    if resultado is not None and resultado.user is not None:
+        # Aquí luego guardaremos la sesión, por ahora solo confirmamos
+        return {"status": "success", "message": f"Bienvenido {resultado.user.email}"}
+    else:
+        # Mostrar nuevamente el login en caso de error
+        return templates.TemplateResponse("login.html", {
+            "request": request, 
+            "mensaje": "Credenciales incorrectas o correo no confirmado."
+        })
--- database/auth_repository.py
+++ database/auth_repository.py
@@ -6,8 +6,11 @@ def register_new_user(email, password, nombre, apellido, telefono):
     Función que usa las funciones del objeto supabase para registrar un usuario.
 
     Entrada
-        - email: Correo único del usuario
+        - email: Correo único del usuario.
         - password: La contraseña hasheada del usuario.
+        - nombre: Nombre del usuario.
+        - apellido: Apellido del usuario.
+        - telefono: Telefono del usuario.
 
     Retorna
         - Una respuesta propia de supabase (201 si se creó el usuario,
@@ -40,4 +43,32 @@ def register_new_user(email, password, nombre, apellido, telefono):
         print(f"Error en el registro: {e}")
 
         # Retornamos none para que se sepa que no funcionó
+        return None
+    
+def login_user(email, password):
+    """
+    Inicia sesión con email y contraseña.
+    Retorna la respuesta de Supabase (con el token de sesión).
+
+    Entrada:
+        - email: El correo del usuario registrado
+        - password: La contraseña del usuario.
+
+    Retorna:
+        - Respuesta de supabase con token configurado para mantener
+        la sesión iniciada.
+    """
+    try:
+        # Realizar la autorización del usuario
+        response = supabase.auth.sign_in_with_password({
+            "email": email, 
+            "password": password
+        })
+        
+        # Retornar respuesta
+        return response
+    
+    except Exception as e:
+        # Imprimir error y retornar nulo
+        print(f"Error en login: {e}")
         return None
